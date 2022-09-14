bf5147543307d521b97292b8e7ef905971e661bc
Karletty Elías <karletty.carolina@gmail.com>
2022-09-14 16:08:27 -0600

    arreglando navBar

--- src/Components/Nav/index.tsx
+++ src/Components/Nav/index.tsx
@@ -1,4 +1,3 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
@@ -6,9 +5,15 @@ import Navbar from 'react-bootstrap/Navbar';
 const Navigation = () => {
     return (
         <Navbar>
-            <Navbar.Brand>Kpelis</Navbar.Brand>
+            <Navbar.Brand><img
+              alt=""
+              src="./logo.png"
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+            />{' '}
+            Kpelis</Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link><i className="fa-solid fa-house"></i></Nav.Link>
                 <Nav.Link>Posts</Nav.Link>
                 <Nav.Link>Recomendations</Nav.Link>
             </Nav>
